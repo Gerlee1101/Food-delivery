6aba44bb5a5315bc66114ab731e71748455dae67
25LP1083 25LP1083 <25LP1083@pinecones-iMac.local>
2025-12-16 12:47:15 +0800

    backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 const app = express();
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("hellodfsdfsdf");
 });
+app.post("/", (req, res) => {});
 const port = 4000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
